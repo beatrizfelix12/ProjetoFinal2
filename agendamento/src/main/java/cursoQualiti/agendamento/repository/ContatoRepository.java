72d693b269e7bbe21e5f6459c3a4a3d83beb49a6
beatrizfelix12 <beatrizfelix443@gmail.com>
2023-10-10 22:17:38 -0300

    commit

--- agendamento/src/main/java/cursoQualiti/agendamento/repository/ContatoRepository.java
+++ agendamento/src/main/java/cursoQualiti/agendamento/repository/ContatoRepository.java
@@ -1,6 +1,6 @@
-package moderna.agendamento.repository;
+package cursoQualiti.agendamento.repository;
 
-import moderna.agendamento.model.Contato;
+import cursoQualiti.agendamento.model.Contato;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
